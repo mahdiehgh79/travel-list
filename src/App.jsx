b4eb62fdeef82a77a6c045614e695124dc505dd9
mahdieh ghasemi <117377128+mahdiehgh79@users.noreply.github.com>
2024-09-30 15:48:51 +0330

    create react-form handlesubmit and new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Sos", quantity: 12, packed: true },
+];
+
 function App() {
   return (
     <div className="app">
@@ -9,20 +15,54 @@ function App() {
   );
 }
 
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(e);
+};
+
 const Logo = () => {
   return <h1>🌴 Far Away 💰 </h1>;
 };
 
 const Form = () => {
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>what do you need for your 🙋 trip?</h3>
-    </div>
+       <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="item ... " />
+      <button>add</button>
+    </form>
   );
 };
 
 const PackingList = () => {
-  return <div className="list">list</div>;
+  return(
+  <div className="list">
+    <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+     </ul>
+  </div>;
+  )
+};
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 };
 
 const Stats = () => {
