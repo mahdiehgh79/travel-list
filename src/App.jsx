66518ed49d84faac2557f9410cf646aa2ecfd5b8
mahdieh ghasemi <117377128+mahdiehgh79@users.noreply.github.com>
2024-10-13 11:51:56 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Form from "./form";
 
 function App() {
   const [items, setItems] = useState([]);
@@ -39,40 +40,7 @@ const Logo = () => {
   return <h1>🌴 Far Away 💰 </h1>;
 };
 
-const Form = () => {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
-  
-  const handleSubmit = (e) => {
-  e.preventDefault();
-  console.log(e);
-   if (!description) return;
 
-    const newItem = { description, quantity, id: Date.now(), packed: false };
-    console.log(newItem);
-
-    onadditems(newItem);
-
-    setDescription("");
-    setQuantity(1);
-};
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>what do you need for your 🙋 trip?</h3>
-       <select value={quantity}
-        onChange={(e) => setQuantity(Number(e.target.value))}>
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input type="text" placeholder="item ... " value={description}
-        onChange={(e) => setDescription(e.target.value)} />
-      <button>add</button>
-    </form>
-  );
-};
 
 const PackingList = ({ items, ondeleteitems, onToggleitem, onClearList }) => {
   const [sortBy, setSortBy] = useState("input");
